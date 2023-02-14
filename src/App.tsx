afffba0adb12f0398dfe8c8aef07fd89f0f5fa7e
Nathaniel Anderson <49240842+NateAnderson500@users.noreply.github.com>
2023-02-14 16:09:24 -0500

    edited some of the messages, and experamented

--- src/App.tsx
+++ src/App.tsx
@@ -24,8 +24,8 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      <p className="Credits">
+        Biggest of ups to all the poeple who made this possible
       </p>
     </div>
   )
