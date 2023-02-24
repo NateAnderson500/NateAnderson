47746050cf7e063a188583e180e0aad11e8b7cb8
Nate Anderson <nja5387@gmail.com>
2023-02-24 12:53:45 -0500

    Removed default code

--- src/App.tsx
+++ src/App.tsx
@@ -7,26 +7,9 @@ function App() {
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="Credits">
-        Biggest of ups to all the poeple who made this possible
-      </p>
+      <header className="App-header">
+        <p>Hello, my name is Nathaniel Anderson!</p>
+      </header>
     </div>
   )
 }
